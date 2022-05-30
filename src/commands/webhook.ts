96f629d2c82c3443e25a7523410a7b4104e58127
Evan <evanpaulbishop@gmail.com>
2022-05-30 01:09:02 -0400

    extracted "webhook" into a const, added desc for exec

--- src/commands/webhook.ts
+++ src/commands/webhook.ts
@@ -1,11 +1,11 @@
 import type { CommandInteraction, TextBasedChannel, Webhook } from "discord.js";
 import { Discord, Slash, SlashGroup, SlashOption } from "discordx";
-
+const group = "webhook";
 @Discord()
-@SlashGroup({ name: "webhook" })
+@SlashGroup({ name: group })
 export class WebHookCommand {
 	@Slash("create", { description: "Create a webhook" })
-	@SlashGroup("webhook")
+	@SlashGroup(group)
 	async create(
 		@SlashOption("name", { type: "STRING" })
 		name: string,
@@ -19,8 +19,8 @@ export class WebHookCommand {
 		interaction.editReply({ content: `Here's your webhook: ${hook.url}` });
 	}
 
-	@Slash("exec")
-	@SlashGroup("webhook")
+	@Slash("exec", { description: "Sends a message using a webhook" })
+	@SlashGroup(group)
 	exec(interaction: CommandInteraction): void {
 		interaction.reply({ content: "NYI", flags: 0b01000000 });
 	}
