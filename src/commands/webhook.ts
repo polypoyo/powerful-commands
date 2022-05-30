8f7d9ae582e7b16d7aa3f4d9edcf40030dd5df82
Evan <evanpaulbishop@gmail.com>
2022-05-30 02:10:11 -0400

    add "/webhook exec" command

--- src/commands/webhook.ts
+++ src/commands/webhook.ts
@@ -1,8 +1,18 @@
-import type { CommandInteraction, TextBasedChannel, Webhook } from "discord.js";
+import {
+	Client,
+	CommandInteraction,
+	TextBasedChannel,
+	Webhook,
+} from "discord.js";
 import { Discord, Slash, SlashGroup, SlashOption } from "discordx";
+//@ts-expect-erro
+import { logPromise } from "../api/logPromise.js";
 const group = "webhook";
+import dotenv from "dotenv";
+import { sleep } from "../api/sleep.js";
+dotenv.config();
 @Discord()
-@SlashGroup({ name: group })
+@SlashGroup({ name: group, description: "Manage webhooks" })
 export class WebHookCommand {
 	@Slash("create", { description: "Create a webhook" })
 	@SlashGroup(group)
@@ -21,7 +31,36 @@ export class WebHookCommand {
 
 	@Slash("exec", { description: "Sends a message using a webhook" })
 	@SlashGroup(group)
-	exec(interaction: CommandInteraction): void {
-		interaction.reply({ content: "NYI", flags: 0b01000000 });
+	async exec(
+		@SlashOption("text", { type: "STRING" })
+		text: string,
+		interaction: CommandInteraction | any
+	): Promise<void> {
+		const hooks: Webhook[] = await interaction.channel?.fetchWebhooks();
+		hooks.forEach((hook, i) => {
+			logPromise(
+				fetch(hook.url, {
+					method: "POST",
+					body: JSON.stringify({ content: `${text}` }),
+					headers: {
+						"Content-Type": "application/json",
+						Accept: "application/json",
+					},
+				})
+			);
+		});
+		const reply = interaction.reply({ content: "Sent!", flags: 0b01000000 });
+		await sleep(1_500);
+		interaction.deleteReply();
+	}
+	@Slash("list", { description: "Lists webhooks in the current channel" })
+	@SlashGroup(group)
+	async list(interaction: CommandInteraction | any): Promise<void> {
+		const hooks: Webhook[] = await interaction.channel?.fetchWebhooks();
+		console.log(hooks);
+		interaction.reply({
+			content: `${"```"}json\n${JSON.stringify(hooks, null, 2)}${"```"}`,
+			flags: 0b01000000,
+		});
 	}
 }
